52e1f1dcb89fbf7d2aa76295804c5354b38748fe
ShahriarHA <110126439+ShahriarHA@users.noreply.github.com>
2022-11-04 20:04:49 +0600

    Add files via upload
    from MR. shera

---
+++ codeWithHarray/home/models.py
@@ -0,0 +1,14 @@
+# from django.db import models
+from django.db import models
+
+
+
+# Create your models here.
+class contact(models.Model):
+    usename = models.CharField(max_length = 122)
+    fname = models.CharField(max_length = 122)
+    lname = models.CharField(max_length = 122)
+    email = models.CharField(max_length = 122)
+    pass1 = models.CharField(max_length = 122)
+    pass2 = models.CharField( max_length= 122)
+    date = models.DateField()
