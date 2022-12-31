7cece176b1b33b6962706e01c80d2bd32e1e55af
mamutalib <mohammadmutalib678@gmail.com>
2023-01-01 00:49:25 +0600

    updated

--- DoctorAppSystem/DoctorAppSystem/info.py
+++
@@ -1,8 +0,0 @@
-from pickle import TRUE
-
-
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'docpa34@gmail.com'
-EMAIL_HOST_PASSWORD = 'password'
-EMAIL_PORT = 587
--- codeWithHarray/home/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from home.models import contact
-from home.models import doctorAccount
-
-# Register your models here.
-admin.site.register(contact)
-admin.site.register(doctorAccount)
